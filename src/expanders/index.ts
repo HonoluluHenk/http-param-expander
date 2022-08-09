881b018623ec50da57ba123cda115bb49eb10b37
Christoph Linder <post@christoph-linder.ch>
2022-08-09 23:27:28 +0200

    add missing expanders to public api

--- src/expanders/index.ts
+++ src/expanders/index.ts
@@ -1,4 +1,6 @@
 export * from './abstract-expander';
-export * from './matrix-param-expander';
+export * from './form-param-expander';
 export * from './label-param-expander';
+export * from './matrix-param-expander';
 export * from './multi-style-expander';
+export * from './simple-param-expander';
